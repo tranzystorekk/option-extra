c44bb32f8327a84fa2313bdeaf116770a3aea537
Marcin Puc <marcin.e.puc@gmail.com>
2021-07-04 15:30:29 +0200

    chore: initial commit

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+//! Additional utilities for the [`Option`] type.
+//!
+//! New methods are added via the [`OptionExt`] trait:
+//!
+//! ```
+//! use option_extra::OptionExt;
+//! ```
+
+/// Extra methods for the [`Option`] type.
+pub trait OptionExt<T> {
+}
