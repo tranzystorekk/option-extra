ff83c02173cf4560d654c8e090d1a25da75cb1d6
Marcin Puc <marcin.e.puc@gmail.com>
2021-07-04 15:32:27 +0200

    feat: add OptionExt::zip_lazy

--- src/lib.rs
+++ src/lib.rs
@@ -8,4 +8,34 @@
 
 /// Extra methods for the [`Option`] type.
 pub trait OptionExt<T> {
+    /// Like [`Option::zip`], but the other [`Option`] is obtained from `f`
+    /// which is not evaluated if `self` is [`None`].
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use option_extra::OptionExt;
+    ///
+    /// fn some() -> Option<i32> { Some(1) }
+    /// fn none() -> Option<i32> { None }
+    ///
+    /// assert_eq!(Some("abc").zip_lazy(some), Some(("abc", 1)));
+    /// assert_eq!(Some("abc").zip_lazy(none), None);
+    /// assert_eq!(None::<i32>.zip_lazy(some), None);
+    /// ```
+    fn zip_lazy<U, F>(self, f: F) -> Option<(T, U)>
+    where
+        F: FnOnce() -> Option<U>;
+}
+
+impl<T> OptionExt<T> for Option<T> {
+    fn zip_lazy<U, F>(self, f: F) -> Option<(T, U)>
+    where
+        F: FnOnce() -> Option<U>,
+    {
+        let a = self?;
+        let b = f()?;
+
+        Some((a, b))
+    }
 }
